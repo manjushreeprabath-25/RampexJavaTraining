e3634b0b1c167967c3ec9c0f64093cd251884b86
MANJU SHREE <manjushreeprabath@gmail.com>
2026-06-29 00:45:36 +0800

    code created

---
+++ src/rampexday7hometask/Numberpattern.java
@@ -0,0 +1,23 @@
+package rampexday7hometask;
+
+public class Numberpattern {
+
+        public static void main(String[] args) {
+
+            for (int i = 1; i <= 5; i++) {
+                for (int j = i; j <= 5; j++) {
+                    System.out.print(j);
+                }
+                System.out.println();
+            }
+
+
+            for (int i = 4; i >= 1; i--) {
+                for (int j = i; j <= 5; j++) {
+                    System.out.print(j);
+                }
+                System.out.println();
+            }
+        }
+    }
+
