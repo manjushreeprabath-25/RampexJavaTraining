c81f666dd44cf7948dee8898cd2429a7984f071e
MANJU SHREE <manjushreeprabath@gmail.com>
2026-06-29 00:36:30 +0800

    code created

---
+++ src/rampexday7hometask/Negativenumber.java
@@ -0,0 +1,19 @@
+package rampexday7hometask;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Negativenumber {
+    public static void main(String[] args) {
+        int []arr={-1,5,7,-89,9,38};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                arr[i]=0;
+            }
+
+        }
+        System.out.println( "Modified array: "+ Arrays.toString(arr));
+
+
+    }
+}
+
