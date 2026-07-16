57c4237acd9d9dbe7316352594b60aa141694f4d
Manju shree P <manjushreeprabath@gmail.com>
2026-07-16 09:43:55 +0530

    java program

---
+++ src/rampexday21/hometask/LongestStringAPI.java
@@ -0,0 +1,17 @@
+package rampexday21.hometask;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class LongestStringAPI {
+    public static void main(String[] args) {
+
+        List<String> list = Arrays.asList("Java", "SpringBoot", "SQL", "Microservices");
+
+        String longest = list.stream()
+                .max(Comparator.comparing(String::length))
+                .get();
+
+        System.out.println(longest);
+    }
+}
